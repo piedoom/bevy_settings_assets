9c90bc20e3d6151bd2de95612b26bc6c1ab93086
Your Name <you@example.com>
2021-01-01 02:24:58 -0600

    working with 0.4 bevy instead of latest

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -1,31 +1,27 @@
 use bevy::prelude::*;
-use bevy_settings_assets::{SettingsPlugin, Settings};
+use bevy_settings_assets::{Settings, SettingsPlugin};
 use serde::Deserialize;
 
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
         .add_plugin(SettingsPlugin::default())
-        .add_startup_system(setup)
-        .add_system(hello_world_system)
+        .add_startup_system(setup.system())
+        .add_system(hello_world_system.system())
         .run();
 }
 
 #[derive(Default, Deserialize)]
 struct MySettingsA {
-    a: u32
+    a: u32,
 }
 
 #[derive(Default, Deserialize)]
 struct MySettingsB {
-    b: String
+    b: String,
 }
 
-fn setup(
-    commands: &mut Commands,
-    asset_server: Res<AssetServer>,
-    mut settings: ResMut<Settings>,
-) {
+fn setup(commands: &mut Commands, asset_server: Res<AssetServer>, mut settings: ResMut<Settings>) {
     settings.add::<MySettingsA>("a.settings", commands, &asset_server);
     settings.add::<MySettingsB>("b.settings", commands, &asset_server);
     asset_server.watch_for_changes().unwrap();
